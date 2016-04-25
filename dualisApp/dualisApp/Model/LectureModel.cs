260c2bc18c0aaafe3114bfd475407db738a16bb9
hmettmann <h.mettmann@gmail.com>
2016-04-25 19:01:11 +0200

    Initial Commit

---
+++ dualisApp/dualisApp/Model/LectureModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace dualisApp.Model
+{
+    public class LectureModel
+    {
+        public DateTime Day { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime EndTime { get; set; }
+
+        public string LectureName { get; set; }
+    }
+}
